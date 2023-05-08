fa035f009ef4fb3a2dbf7c4766bc2b6302fe205e
LilliC2 <lillichuawiwat@gmail.com>
2023-05-08 10:47:30 +1000

    Snap Counter Text for each layer

---
+++ Prototype_5/Assets/Scripts/Enemies/EnemyManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : GameBehaviour<EnemyManager>
+{
+    public List<GameObject> EnemyList;
+    public GameObject trollPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnEnemies()
+    {
+        
+    }
+}
